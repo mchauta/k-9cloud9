eeb49982cc993efd442a34ec5a65c119b3356f6f
Matt Chauta <mchauta@nrao.edu>
2018-12-17 11:42:18 -0500

    setting up page template

---
+++ basic.php
@@ -0,0 +1,75 @@
+<?php
+/**
+* Template Name: Basic
+ */
+
+ $has_banner = get_field('has_top_banner');
+ $subHeading = get_field('sub_heading');
+ get_header(); ?>
+
+
+ <?php while ( have_posts() ) : the_post(); ?>
+ 	<?php if (get_field('has_top_banner')): ?>
+ 		<div id="page-sub-header" style="background-image:url(<?php echo get_the_post_thumbnail_url();?>)">
+ 				<div class="container">
+ 						<h1>
+ 								<?php
+ 								the_title();
+ 								?>
+ 						</h1>
+ 						<p>
+ 								<?php
+ 								echo $subHeading;
+ 								?>
+ 						</p>
+ 						<a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a>
+ 				</div>
+ 		</div>
+ 	<?php endif; ?>
+ 	<section id="primary" class="content-area col-sm-12 col-lg-8">
+ 		<main id="main" class="site-main" role="main">
+
+ 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+ 				<?php
+ 					$enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
+ 					if(!$enable_vc ) {
+ 					?>
+ 					<header class="entry-header">
+ 					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+ 				</header><!-- .entry-header -->
+ 					<?php } ?>
+
+ 				<div class="entry-content">
+ 					<?php
+ 						the_content();
+
+ 						wp_link_pages( array(
+ 							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-bootstrap-starter' ),
+ 							'after'  => '</div>',
+ 						) );
+ 					?>
+ 				</div><!-- .entry-content -->
+
+ 				<?php if ( get_edit_post_link() && !$enable_vc ) : ?>
+ 					<footer class="entry-footer">
+ 						<?php
+ 							edit_post_link(
+ 								sprintf(
+ 									/* translators: %s: Name of current post */
+ 									esc_html__( 'Edit %s', 'wp-bootstrap-starter' ),
+ 									the_title( '<span class="screen-reader-text">"', '"</span>', false )
+ 								),
+ 								'<span class="edit-link">',
+ 								'</span>'
+ 							);
+ 						?>
+ 					</footer><!-- .entry-footer -->
+ 				<?php endif; ?>
+ 			</article><!-- #post-## -->
+ 		<?php endwhile; ?>
+
+ 		</main><!-- #main -->
+ 	</section><!-- #primary -->
+
+ <?php
+ get_footer();
--- page.php
+++ page.php
@@ -12,74 +12,27 @@
  * @package WP_Bootstrap_Starter
  */
 
-
-$has_banner = get_field('has_top_banner');
-$subHeading = get_field('sub_heading');
 get_header(); ?>
 
-
-<?php while ( have_posts() ) : the_post(); ?>
-	<?php if (get_field('has_top_banner')): ?>
-		<div id="page-sub-header">
-				<div class="container">
-						<h1>
-								<?php
-								the_title();
-								?>
-						</h1>
-						<p>
-								<?php
-								echo $subHeading;
-								?>
-						</p>
-						<a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a>
-				</div>
-		</div>
-	<?php endif; ?>
 	<section id="primary" class="content-area col-sm-12 col-lg-8">
 		<main id="main" class="site-main" role="main">
 
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php
-					$enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
-					if(!$enable_vc ) {
-					?>
-					<header class="entry-header">
-					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-				</header><!-- .entry-header -->
-					<?php } ?>
+			<?php
+			while ( have_posts() ) : the_post();
 
-				<div class="entry-content">
-					<?php
-						the_content();
+				get_template_part( 'template-parts/content', 'page' );
 
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-bootstrap-starter' ),
-							'after'  => '</div>',
-						) );
-					?>
-				</div><!-- .entry-content -->
+                // If comments are open or we have at least one comment, load up the comment template.
+                if ( comments_open() || get_comments_number() ) :
+                    comments_template();
+                endif;
 
-				<?php if ( get_edit_post_link() && !$enable_vc ) : ?>
-					<footer class="entry-footer">
-						<?php
-							edit_post_link(
-								sprintf(
-									/* translators: %s: Name of current post */
-									esc_html__( 'Edit %s', 'wp-bootstrap-starter' ),
-									the_title( '<span class="screen-reader-text">"', '"</span>', false )
-								),
-								'<span class="edit-link">',
-								'</span>'
-							);
-						?>
-					</footer><!-- .entry-footer -->
-				<?php endif; ?>
-			</article><!-- #post-## -->
-		<?php endwhile; ?>
+			endwhile; // End of the loop.
+			?>
 
 		</main><!-- #main -->
 	</section><!-- #primary -->
 
 <?php
+get_sidebar();
 get_footer();
