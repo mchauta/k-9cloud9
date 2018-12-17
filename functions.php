4434ec3879c3a84f17e3289a5bd51ace4fbc0132
Matt Chauta <mchauta@nrao.edu>
2018-12-17 10:25:02 -0500

    adding debug template statement

--- functions.php
+++ functions.php
@@ -1,1 +1,9 @@
 <?php
+
+//debug the template name being used
+add_action('wp_head', 'k9_show_template');
+function k9_show_template() {
+    global $template;
+    $template_name= basename($template);
+    if ( class_exists( 'PC' ) ) { PC::debug($template_name, 'The template being used:');}
+}
