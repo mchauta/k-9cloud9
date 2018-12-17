d76ab40a8deb43e6cbaae6dd78b47b644629f853
Matt Chauta <mchauta@nrao.edu>
2018-12-17 10:42:28 -0500

    change name

--- functions.php
+++ functions.php
@@ -1,8 +1,8 @@
 <?php
 
 //debug the template name being used
-add_action('wp_head', 'k9_show_template');
-function k9_show_template() {
+add_action('wp_head', 'mchauta_show_template');
+function mchauta_show_template() {
     global $template;
     $template_name= basename($template);
     if ( class_exists( 'PC' ) ) { PC::debug($template_name, 'The template being used:');}
