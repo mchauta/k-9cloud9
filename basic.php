f2f3ed11027fd99a45907a482fcaed32239b7048
Matt Chauta <mchauta@nrao.edu>
2018-12-17 12:04:43 -0500

    adding footer address

--- basic.php
+++ basic.php
@@ -29,7 +29,7 @@
 	<div id="content" class="site-content">
 		<div class="container">
 			<div class="row">
- 	<section id="primary" class="content-area col-sm-12 col-lg-8">
+ 	<section id="primary" class="content-area col-sm-12">
  		<main id="main" class="site-main" role="main">
 
  			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
