fd0f42f8d55440cf1ba6d999d9f1bd9a7933c0c7
Matt Chauta <mchauta@nrao.edu>
2019-01-04 12:21:28 -0500

    more tweaks

--- header.php
+++ header.php
@@ -27,16 +27,13 @@
 	<header id="masthead" class="site-header navbar-static-top <?php echo wp_bootstrap_starter_bg_class(); ?>" role="banner">
         <div class="container">
             <nav class="navbar navbar-expand-xl p-0">
+
                 <div class="navbar-brand">
-                  <div class="brand-pad">
-                    <?php if ( get_theme_mod( 'wp_bootstrap_starter_logo' ) ): ?>
-                        <a href="<?php echo esc_url( home_url( '/' )); ?>">
-                            <img src="<?php echo esc_attr(get_theme_mod( 'wp_bootstrap_starter_logo' )); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>">
-                        </a>
-                    <?php else : ?>
-                        <a class="site-title" href="<?php echo esc_url( home_url( '/' )); ?>"><?php esc_url(bloginfo('name')); ?></a>
-                    <?php endif; ?>
-                  </div>
+                  <a href="<?php echo esc_url( home_url( '/' )); ?>">
+                    <div class="brand-pad">
+
+                    </div>
+                  </a>
                 </div>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-nav" aria-controls="" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
