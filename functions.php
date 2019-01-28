6ee1e309213ad47f7f2b9ca7aa594c531ba1cf6b
Matt Chauta <mchauta@nrao.edu>
2019-01-28 11:24:46 -0500

    1st round edits

--- functions.php
+++ functions.php
@@ -31,7 +31,7 @@ function display_phone_element()
 function display_address_element()
 {
 	  ?>
-    	<textarea type="textarea" rows='7' cols='50' name="address_field" id="address_field" value="<?php echo get_option('address_field'); ?>" ></textarea>
+    	<input type="textarea" rows='7' cols='50' name="address_field" id="address_field" value="<?php echo get_option('address_field'); ?>" />
     <?php
 }
 
