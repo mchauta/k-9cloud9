474ce250ec86ed9533dd65b40a892a9a4969ea0d
Matt Chauta <mchauta@nrao.edu>
2019-02-13 11:22:18 -0500

    updating break points

--- 404.php
+++ 404.php
@@ -12,8 +12,6 @@ $thumb = false;
 
 get_header(); ?>
 
-
- <?php while ( have_posts() ) : the_post(); ?>
  	<?php if ($has_banner): ?>
  		<div id="page-sub-header" style="background-image:url(<?php if ($thumb) : echo $thumb; else :  img_path('pattern.svg') ; endif;?>)">
  				<div class="container">
@@ -24,8 +22,6 @@ get_header(); ?>
  						</p>
           </div>
             <div class="cta-container">
-              <?php if (get_field('has_phone')): ?><a href="tel:+1<?php echo $phone; ?>" class="cta-button"><?php echo $phoneFormat; ?></a><?php endif; ?>
-              <?php if (get_field('has_book')): ?><a href="<?php echo get_option('book_url'); ?>" class="cta-button">Book Now</a><?php endif; ?>
             </div>
  						<a href="#content" class="page-scroller"><i class="fa fa-fw fa-angle-down"></i></a>
  				</div>
@@ -38,45 +34,13 @@ get_header(); ?>
  		<main id="main" class="site-main" role="main">
 
  			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
- 				<?php
- 					$enable_vc = get_post_meta(get_the_ID(), '_wpb_vc_js_status', true);
- 					if(!$enable_vc ) {
- 					?>
-					<?php if (!$has_banner): ?>
-	 					<header class="entry-header">
-	 						<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-	 					</header><!-- .entry-header -->
-					<?php endif; ?>
- 					<?php } ?>
 
  				<div class="entry-content">
- 					<?php
- 						the_content();
-
- 						wp_link_pages( array(
- 							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-bootstrap-starter' ),
- 							'after'  => '</div>',
- 						) );
- 					?>
+ 					?
  				</div><!-- .entry-content -->
 
- 				<?php if ( get_edit_post_link() && !$enable_vc ) : ?>
- 					<footer class="entry-footer">
- 						<?php
- 							edit_post_link(
- 								sprintf(
- 									/* translators: %s: Name of current post */
- 									esc_html__( 'Edit %s', 'wp-bootstrap-starter' ),
- 									the_title( '<span class="screen-reader-text">"', '"</span>', false )
- 								),
- 								'<span class="edit-link">',
- 								'</span>'
- 							);
- 						?>
- 					</footer><!-- .entry-footer -->
- 				<?php endif; ?>
+
  			</article><!-- #post-## -->
- 		<?php endwhile; ?>
 
  		</main><!-- #main -->
  	</section><!-- #primary -->
